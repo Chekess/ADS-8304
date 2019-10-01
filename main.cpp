dd87620eb3fdf1c873d2a84c6e8d3bb4c81aa864
Xenia Chechetkina <42899018+Chekess@users.noreply.github.com>
2019-10-02 01:43:54 +0300

    [small FIX][?][?] Chechetkina lab1 changed

--- main.cpp
+++ main.cpp
@@ -1,98 +1,80 @@
 #include <iostream>
 #include <string>
 
-class MyRecursion{
-private:
-    std::string str;
-public:
-    int length;
-    int Recursion(int, int);
-    void read();
-    void write(std::string);
-    void test(int ,int );
-};
-
-void MyRecursion::read() {
-    std::cin >> str;
-    length = str.length()-1;
-}
-
-void MyRecursion::write(std::string MyText) {
-    std::cout << MyText << "\n";
-}
-
-void MyRecursion::test(int l,int r){
-    for(int i=l;i<=r;i++)
-        std::cout<<str[i];
-    write("");
-}
-
-int MyRecursion::Recursion(int left, int right) {
-
-    test(left,right);
-
-    if (left>=right)
-    {
-        write("yes");
-        return 0;
-    }
-
-    bool flag = true;
-    int k = 0;
-
-    for (k; k < 3; k++) {
-        if (!isalpha(str[left+k])) {
-            flag = false;
-        }
-    }
-
-    if (str[left+k] != '=') {
-        flag = false;
-    }
-    k++;
-
-    for (k; k < 6; k++) {
-        if (!isalnum(str[left+k])) {
-            flag = false;
-        }
-    }
-
-    if (flag){
-        if ((isalnum(str[left+6])!=0 || isalpha(str[left+6]!=0))){
-            write("no");
-            return 0;
-        }
-        Recursion(left+((str[left+6] = ',')? 7:6),right);
-        return 0;
-    }
-
-    flag = true;
-    k = 0;
-
-    for (k; k < 3; k++) {
-        if (!isalpha(str[left+k])) {
-            flag = false;
-        }
-    }
-
-    if ((!flag) || (str[left+3] != '=') || (str[left+4] != '(') || (str[right] != ')')){
-        flag = false;
-    }
-
-    if (flag){
-        Recursion(left+5,right-1);
-        return 0;
-    } else {
-        write("no");
-        return 0;
-    }
-}
-
-
+void TrySimplePar(std::string);
 
 int main() {
-    MyRecursion MySimple;
-    MySimple.read();
-    MySimple.Recursion(0,MySimple.length);
+    std::string str;
+    std::cin >> str;
+
+    TrySimplePar(str);
+    //std::cout << "Не список параметров" << "\n";
+
     return 0;
+}
+
+void TrySimplePar(std::string str)
+{
+    int StartLengh = str.length();
+
+    for (int i=0;i<str.length() - 6;i++) {
+        bool flag = true;
+        int k = 0;
+
+        for (k; k < 3; k++) {
+            if (!isalpha(str[i+k])) {
+                flag = false;
+            }
+        }
+        if (!flag) { continue;}
+
+        if (str[i+k] != '=') {
+            flag = false;
+        }
+        k++;
+        if (!flag) { continue;}
+
+        for (k; k < 7; k++) {
+            if (!isalnum(str[i+k])) {
+                flag = false;
+            }
+        }
+
+        if (flag)
+        {
+            str.replace(i,7,"*");
+        }
+    }
+
+    for (int i=0;i<str.length() - 2;i++) {
+        if ((str[i] == '*') && (str[i+1] == ',') && (str[i+2] == '*')) {
+            str.replace(i,3,"*");
+        }
+    }
+
+    for (int i=0;i<str.length() - 6;i++) {
+
+        bool flag = true;
+        int k = 0;
+
+        for (k; k < 3; k++) {
+            if (!isalpha(str[i+k])) {
+                flag = false;
+            }
+        }
+
+        if ((flag) && (str[i+3] == '=') && (str[i+4] == '(') && (str[i+5] == '*') && (str[i+6] == ')'))
+        {
+            str.replace(i,7,"*");
+        }
+    }
+
+    if (str.length() == StartLengh) {
+        std::cout << "Не список параметров" << "\n";
+    }else if ((str.length() == 1) && (str[0] == '*')){
+        std::cout << "Cписок параметров" << "\n";
+    }else{
+        TrySimplePar(str);
+    }
+
 }
