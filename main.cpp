3d7b7a0c78b14a078758e1ee5c7cd2d451357d6b
Xenia Chechetkina <42899018+Chekess@users.noreply.github.com>
2019-10-07 23:15:28 +0300

    Chechetkina_lab1

--- main.cpp
+++ main.cpp
@@ -1,95 +1,142 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <conio.h>
 
 class MyRecursion{
 private:
     std::string str;
 public:
     int length;
-    int Recursion(int, int);
-    void read();
-    void test(int,int );
+    int recursion(int, int);
+    void read(char* );
+    void testWorkingDistance(int, int);
+    void write(std::string);
+MyRecursion();
+MyRecursion(std::string&);
 };
 
-void MyRecursion::read() {
-    std::cin >> str;
+void MyRecursion::write(std::string result){
+    std::cout << result << std::endl;
+}
+
+void MyRecursion::read(char* text) {
+    str = text;
     length = str.length()-1;
 }
 
 
-void MyRecursion::test(int leftBorder,int rightBorder){
-    for(int i=leftBorder;i<=rightBorder;i++)
+void MyRecursion::testWorkingDistance(int leftBorder,int rightBorder){
+    for(int i = leftBorder; i <= rightBorder; i++)
         std::cout << str[i];
-    std::cout<<"\n";
+    std::cout << std::endl;
+
 }
 
-int MyRecursion::Recursion(int leftBorder, int rightBorder) {
+MyRecursion::MyRecursion(std::string &string) {
+    this->str = string;
+    this->length = str.length()-1;
+}
 
-    test(leftBorder,rightBorder);
+MyRecursion::MyRecursion() {
+    this->str = "";
+    this->length = 0;
+}
 
-    if (leftBorder>=rightBorder)
+int MyRecursion::recursion(int leftBorder, int rightBorder) {
+
+    testWorkingDistance(leftBorder, rightBorder);
+
+    if (leftBorder >= rightBorder)
     {
-        std::cout<<"yes";
+        write("it's a parameter list");
         return 0;
     }
 
-    bool flag = true;
-    int symbCount = 0;
+    bool verification = true;
+    int symbolCount = 0;
 
-    for (symbCount; symbCount < 3; symbCount++) {
-        if (!isalpha(str[leftBorder+symbCount])) {
-            flag = false;
+    for (symbolCount; symbolCount < 3; symbolCount++) {
+        if (!isalpha(str[leftBorder + symbolCount])) {
+            verification = false;
         }
     }
 
-    if (str[leftBorder+symbCount] != '=') {
-        flag = false;
+    if (str[leftBorder + symbolCount] != '=') {
+        verification = false;
     }
-    symbCount++;
+    symbolCount++;
 
-    for (symbCount; symbCount < 6; symbCount++) {
-        if (!isalnum(str[leftBorder+symbCount])) {
-            flag = false;
+    for (symbolCount; symbolCount < 6; symbolCount++) {
+        if (!isalnum(str[leftBorder + symbolCount])) {
+            verification = false;
         }
     }
 
-    if (flag){
-        if ((isalnum(str[leftBorder+6])!=0 || isalpha(str[leftBorder+6]!=0))){
-            std::cout<<"no";
+    if (verification){
+        if ((isalnum(str[leftBorder + 6])!= 0 || isalpha(str[leftBorder + 6] != 0))){
+            write("it's not a parameter list");
             return 0;
         }
-        Recursion(leftBorder+((str[leftBorder+6] = ',')? 7:6),rightBorder);
+        recursion(leftBorder + ((str[leftBorder + 6] = ',') ? 7:6), rightBorder);
         return 0;
     }
 
-    flag = true;
-    symbCount = 0;
+    verification = true;
+    symbolCount = 0;
 
-    for (symbCount; symbCount < 3; symbCount++) {
-        if (!isalpha(str[leftBorder+symbCount])) {
-            flag = false;
+    for (symbolCount; symbolCount < 3; symbolCount++) {
+        if (!isalpha(str[leftBorder + symbolCount])) {
+            verification = false;
         }
     }
 
-    if ((!flag) || (str[leftBorder+3] != '=') || (str[leftBorder+4] != '(') || (str[rightBorder] != ')')){
-        flag = false;
+    bool notEqualSign = str[leftBorder + 3] != '=';
+    bool notLeftBracket = str[leftBorder + 4] != '(';
+    bool notRightBracket = str[rightBorder] != ')';
+
+    if ((!verification) || notEqualSign || notLeftBracket || notRightBracket){
+        verification = false;
     }
 
-    if (flag){
-        Recursion(leftBorder+5,rightBorder-1);
-        return 0;
+    if (verification){
+        recursion(leftBorder + 5,rightBorder - 1);
     } else {
-        std::cout<<"no";
-        return 0;
+        write("it's not a parameter list");
     }
+    return 0;
 }
 
 
 
-int main() {
-    MyRecursion Text;
-    std::cout<<"Enter string"<<"\n";
-    Text.read();
-    Text.Recursion(0,Text.length);
+int main(int argc, char* argv[]) {
+
+    if (argc == 1){
+        std::cout << "argc: " << argc << std::endl;
+        std::cout << "argv[0]: " << argv[0] << std::endl;
+       // std::cout << "argv[1]:" << argv[1] << std::endl;
+        std::cout << "enter" << std::endl;
+        std::string line;
+        std::ifstream in;
+        in.open("D:\\study\\test.txt");
+        if (in.is_open())
+        {
+            std::cout << "file opened" << std::endl;
+            while (getline(in, line))
+            {
+                MyRecursion Text(line);
+                Text.recursion(0, Text.length);
+                std::cout<<std::endl;
+            }
+        }
+        in.close();//закрываем файл
+    }else{
+        std::cout << argv[1] << std::endl;
+        MyRecursion Text;//(argv[1]);
+    //std::cout << "Enter string" << "\n";
+        Text.read(argv[1]);
+        Text.recursion(0, Text.length);
+
+    }
     return 0;
 }
